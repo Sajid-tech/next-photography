675a2724308e5be5aa958bae8c924894f5141005
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-11-26 22:53:46 +0530

    ui compkete and function

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-import Navbar from '@/components/Navbar'
+import Hero from '@/components/Hero'
+import Instagram from '@/components/Instagram'
+import Slider from '@/components/Slider'
+import { SliderData } from '@/components/SliderData'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
 
+      <Hero heading="Photoverse" message="I capture momemts in nature and keep them alive" />
+      <Slider slides={SliderData} />
+      <Instagram />
     </div>
   )
 }
